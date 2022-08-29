70bf6ad130e122b76cd89635b291a47263b334e2
samiul19 <106384192+samiul19@users.noreply.github.com>
2022-08-29 21:33:42 +0600

    assignment complete

--- public/assets/js/nobir.js
+++ public/assets/js/nobir.js
@@ -1,4 +1,12 @@
-$("#teacher_id").onChange(function () {
-  var id = $(this).val();
-  console.log(id);
+// $("#teacher_id").onChange(function () {
+//   var id = $(this).val();
+//   console.log(id);
+// });
+console.log("1");
+// $("#confirm_password").on("change", function () {
+//   alert("The text has been changed.");
+//   console.log("12");
+// });
+$(document.body).on("change", "#confirm_password", function () {
+  alert("Change Happened");
 });
